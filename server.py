69654b6537b990fbefd4072f4df7d17db1a1ca29
Timothy Garvin <timothygarvin4@gmail.com>
2023-05-04 14:33:24 -0700

    Routing Practice Flask

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello World!"
+
+@app.route("/dojo")
+def dojo():
+    return "Dojo!"
+
+@app.route("/say/<string:name>")
+def say(name):
+    return f"Hi, {name}!"
+
+@app.route("/repeat/<int:num>/<string:word>")
+def repeat(num, word):
+    return f"{word * num}"
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True, host="localhost", port=5001)
